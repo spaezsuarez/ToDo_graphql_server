021e2eff757945979aa5af2ba056a52f7d986480
spaezsuarez <spaezsuarez@gmail.com>
2021-05-10 10:35:01 -0500

    Estructura inicial de la clase para la conexión y realización de operaciones en la base de datos

---
+++ src/db/MongoConection.ts
@@ -0,0 +1,16 @@
+import User from '../models/User';
+import Task from '../models/Task';
+import ItemTask from '../models/ItemTask';
+
+export default class MongoConnection {
+
+    public constructor() {
+
+    }
+
+   
+
+    
+
+
+}
