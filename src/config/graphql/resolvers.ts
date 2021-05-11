0c490552f775567a7b57df6d40720ccdc011d491
spaezsuarez <spaezsuarez@gmail.com>
2021-05-11 13:26:26 -0500

    Query y Mutatin para operaciones del backend v0.1

--- src/config/graphql/resolvers.ts
+++ src/config/graphql/resolvers.ts
@@ -1,18 +1,23 @@
 import { IResolvers } from 'graphql-tools';
 import MongoConnection from '../../db/MongoConection';
+import Task from '../../models/Task';
 import User from '../../models/User';
 
 let databaseConnection = new MongoConnection();
 
 export const resolvers:IResolvers = {
     Query:{
-        getTasks:() => { return {} },
-        getUsers:async () => { return await databaseConnection.getUsers(); }
+        getUser: (_:any,{ user }) => { return databaseConnection.getOneUser(user) },
+        getUsers: async () => { return await databaseConnection.getUsers(); }
     },
     Mutation:{
-        createUser:async (_:any,{ userInput }) => {
-            let user:User = new User(userInput.id,userInput.name,userInput.password);
+        createUser: async (_:any,{ userInput }) => {
+            let user:User = new User(userInput.name,userInput.password);
             return await databaseConnection.createUser(user);
+        },
+        createTask: async (_:any,{ idUser,taskInput }) => {
+            let task:Task = new Task(taskInput.title,taskInput.description,taskInput.isDone,taskInput.startDate,taskInput.endDate,taskInput.itemsTasks);
+            return await databaseConnection.createTask(idUser,task);
         }
     }
 
