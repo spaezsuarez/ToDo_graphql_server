f9150e7062e7a5ba8a951892f3beb0de90a7b14c
spaezsuarez <spaezsuarez@gmail.com>
2021-05-10 19:59:16 -0500

    Agregando mutaciones al esquema de graphql

--- src/config/graphql/resolvers.ts
+++ src/config/graphql/resolvers.ts
@@ -1,12 +1,19 @@
 import { IResolvers } from 'graphql-tools';
 import MongoConnection from '../../db/MongoConection';
+import User from '../../models/User';
 
 let databaseConnection = new MongoConnection();
 
 export const resolvers:IResolvers = {
     Query:{
         getTasks:() => { return {} },
-        getUsers:() => { return databaseConnection.getUser() }
+        getUsers:async () => { return await databaseConnection.getUsers(); }
     },
+    Mutation:{
+        createUser:async (_:any,{ userInput }) => {
+            let user:User = new User(userInput.id,userInput.name,userInput.password);
+            return await databaseConnection.createUser(user);
+        }
+    }
 
 }
