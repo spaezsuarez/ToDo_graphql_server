0227894bdd590824407a21959223691ffb80825e
spaezsuarez <spaezsuarez@gmail.com>
2021-05-10 20:00:17 -0500

    Operaciones crud del usuario (Aun sin terminar)

--- src/db/MongoConection.ts
+++ src/db/MongoConection.ts
@@ -1,26 +1,48 @@
+//Modelos de negocio
 import User from '../models/User';
 import Task from '../models/Task';
 import ItemTask from '../models/ItemTask';
-import { model } from 'mongoose';
+//Schemas de la base de datos
+import { itemModel } from './schemas/itemTaskSchema';
+import { taskModel  } from './schemas/TaskSchema';
+import { userModel } from './schemas/UserSchema';
 
 export default class MongoConnection {
 
-    public constructor() {
-
+    public getUser(id:any):Promise<User|any>{
+        return userModel.findOne({_id:id}).then((user:any) => {
+            let temp:User = new User(user._id,user.name,user.password);
+            temp.setTasks(user.tasks);
+            return temp;
+        }).catch( () => {
+            return {}
+        });
     }
 
-    public getUser():User{
-        let user:User = new User(1,"Sergio Paez");;
+    public getUsers():Promise<User[]>{
+        return userModel.find().then((users:any[]) => {
+            let arrayUsers:User[] = [];
+            for(let e of users){
+                let temp:User = new User(e._id,e.name,e.password);
+                temp.setTasks(e.tasks);
+                arrayUsers.push(temp);
+            }
+            return arrayUsers;
+
+        }).catch( () => {return []} );
+    }
+
+    public createUser(user:User):User{
+        const dataUser = new userModel(user);
+        dataUser.save();
         return user;
     }
 
-    public createUser(user:User){
+    public updateUser(){
 
     }
 
-   
 
-    
 
 
 }
