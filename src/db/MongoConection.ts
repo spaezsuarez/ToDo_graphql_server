a0592c6fb5539ef99e901f21950ec2e9a024b541
spaezsuarez <spaezsuarez@gmail.com>
2021-05-11 13:27:33 -0500

    Metodos para creación,obtención y actualización de usuarios y metodo para la creación de tareas

--- src/db/MongoConection.ts
+++ src/db/MongoConection.ts
@@ -3,27 +3,26 @@ import User from '../models/User';
 import Task from '../models/Task';
 import ItemTask from '../models/ItemTask';
 //Schemas de la base de datos
-import { itemModel } from './schemas/itemTaskSchema';
-import { taskModel  } from './schemas/TaskSchema';
-import { userModel } from './schemas/UserSchema';
+import { ItemModel } from './schemas/itemTaskSchema';
+import { TaskModel  } from './schemas/TaskSchema';
+import { UserModel } from './schemas/UserSchema';
 
 export default class MongoConnection {
 
-    public getUser(id:any):Promise<User|any>{
-        return userModel.findOne({_id:id}).then((user:any) => {
-            let temp:User = new User(user._id,user.name,user.password);
-            temp.setTasks(user.tasks);
-            return temp;
-        }).catch( () => {
-            return {}
-        });
+    public async getOneUser(data:any):Promise<User|any>{
+        let userMongo:any = await UserModel.findOne({name:data.name,password:data.password}).populate('tasks').exec();
+        let temp:User = new User(userMongo.name,userMongo.password);
+        temp.setId(userMongo._id);
+        temp.setTasks(userMongo.tasks);
+        return temp;
     }
 
     public getUsers():Promise<User[]>{
-        return userModel.find().then((users:any[]) => {
+        return UserModel.find().then((users:any[]) => {
             let arrayUsers:User[] = [];
             for(let e of users){
-                let temp:User = new User(e._id,e.name,e.password);
+                let temp:User = new User(e.name,e.password);
+                temp.setId(e._id);
                 temp.setTasks(e.tasks);
                 arrayUsers.push(temp);
             }
@@ -32,17 +31,31 @@ export default class MongoConnection {
         }).catch( () => {return []} );
     }
 
-    public createUser(user:User):User{
-        const dataUser = new userModel(user);
-        dataUser.save();
+    public async createUser(user:User):Promise<User>{
+        const dataUser = new UserModel(user);
+        let { _id } = await dataUser.save();
+        user.setId(_id);
         return user;
     }
 
-    public updateUser(){
-
+    public async updateUser(userData:User):Promise<any>{
+        let user:any = await UserModel.findOne({_id:userData.getId()});
+        for (let clave in user) {
+            if (userData.hasOwnProperty(clave)) {
+               user[clave] = userData[clave];
+            }
+        }
+        user.save();
+        return user;
     }
 
-
-
+    public async createTask(idUser:string,task:Task):Promise<Task>{
+        const taskData = new TaskModel(task);
+        let dataTask:any = await taskData.save();
+        let user:any = await UserModel.findOne({_id:idUser});
+        user.tasks.push(dataTask._id);
+        await user.save();
+        return task;
+    }
 
 }
