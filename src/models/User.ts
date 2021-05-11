b3e50558af212f9e29e9dc9213fd3229c2b804a6
spaezsuarez <spaezsuarez@gmail.com>
2021-05-10 19:59:53 -0500

    Agregando cambios del modelo

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,12 +4,18 @@ export default class User{
 
     public id: string | number;
     private name:string;
+    private password:string;
     private tasks: Task[];
 
-    public constructor(id:string | number, name:string){
+    public constructor(id:string | number, name:string,password:string){
         this.id = id;
         this.name = name;
+        this.password = password;
         this.tasks = [];
+    }
+
+    public setTasks(tasks:Task[]):void{
+        this.tasks = tasks;
     }
 
     public getId():string | number{
@@ -22,6 +28,10 @@ export default class User{
 
     public getTasks():Task[]{
         return this.tasks;
+    }
+
+    public getPassword():string{
+        return this.password;
     }
 
 }
