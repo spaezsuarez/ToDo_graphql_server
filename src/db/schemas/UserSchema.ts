8942680e1d08dc36049940c48c3240ba1efd62a3
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-07-01 21:52:26 -0500

    Eliminando uso de Mngodb

--- src/db/schemas/UserSchema.ts
+++
@@ -1,9 +0,0 @@
-import { model,Schema } from 'mongoose';
-
-const UserSchema:Schema = new Schema({
-    name:{type:String,required:true},
-    password:{type:String,required:true},
-    tasks:[{type: Schema.Types.ObjectId,ref: 'Task'}]
-});
-
-export const UserModel = model('User',UserSchema);
--- src/db/schemas/itemTaskSchema.ts
+++
@@ -1,9 +0,0 @@
-import { model,Schema } from 'mongoose';
-
-const itemTask:Schema = new Schema({
-    id:{type:Number,required:true},
-    isDone:{type:Boolean,required:true},
-    text:{type:String,required:true}
-});
-
-export const ItemModel = model('ItemTask',itemTask);
